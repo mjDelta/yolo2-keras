9de16eee1e87293ec3dde3bf17d8d6c1249f42f7
Mengji Zhang <zmj_delta@163.com>
2018-05-25 11:39:36 +0800

    add decode_netout method

--- utils.py
+++ utils.py
@@ -51,7 +51,56 @@ def interval_overlap(args1,args2):
     return 0
   else:
     return min(max1,max2)-max(min1,min2)
+  
+def sigmoid(x):
+  return 1./(1+np.exp(-x))
 
+def softmax(x,axis=-1,t=-100):
+  x=x-np.max(x)
+  if np.min(x)<t:
+    x=x/np.min(x)*t
+  e_x=np.exp(x)
+  return e_x/e_x.sum(axis,keep_dims=True)
+
+def decode_netout(netout,obj_th,nms_th,anchors,nb_class):
+  grid_h,grid_w,nb_box=netout.shape[:3]
+  boxes=[]
+  ##decode the output
+  netout[...,4]=sigmoid(netout[...,4])
+  netout[...,5:]=netout[...,4][...,np.newaxis]*softmax(netout[...,5:])
+  netout[...,5:]*=netout[...,5:]>obj_th
+  
+  for row in range(grid_h):
+    for col in range(grid_w):
+      for b in range(nb_box):
+        classes=netout[row,col,b,5:]
+        
+        if np.sum(classes)>0:
+          x,y,w,h=netout[row,col,b,:4]
+          
+          x=(col+sigmoid(x))/grid_w
+          y=(row+sigmoid(y))/grid_h
+          w=anchors[2*b]*np.exp(w)/grid_w
+          h=anchors[2*b+1]*np.exp(h)/grid_h
+          confidence=netout[row,col,b,4]
+          
+          box=BndBox(x,y,w,h,confidence,classes)
+          boxes.append(box)
+  for c in range(nb_class):
+    sorted_indices=list(reversed(np.argsort([box.classes[c] for box in boxes])))
+    
+    for i in range(len(sorted_indices)):
+      index_i=sorted_indices[i]
+      if boxes[index_i].classes[c]==0:
+        continue
+      else:
+        for j in range(i+1,len(sorted_indices)):
+          index_j=sorted_indices[j]
+          
+          if bbox_iou(boxes[index_i],boxes[index_j])>=nms_th:
+            boxes[index_j].classes[c]=0
+  boxes=[box for box in boxes if box.get_score()>obj_th]
+  return boxes
 if __name__=="__main__":
   a=BndBox(30,30,40,40)
   b=BndBox(45,45,50,50)
