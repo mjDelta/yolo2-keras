9079f36c4b091aad0d574055bebac5f033184d61
Mengji Zhang <zmj_delta@163.com>
2018-05-26 17:13:31 +0800

    add self-defined lrelu to save memory

--- backend.py
+++ backend.py
@@ -1,5 +1,16 @@
-from keras.layers import Input,BatchNormalization,LeakyReLU,Conv2D,MaxPooling2D
+from keras.layers import Input,BatchNormalization,Conv2D,MaxPooling2D,Lambda
 from keras.models import Model
+import tensorflow as tf
+
+##add leakyrelu from https://github.com/tensorflow/tensorflow/issues/4079, for saving memory
+def lrelu(x,alpha=0.1,name="lrelu"):
+  with tf.variable_scope(name):
+    f1=0.5*(1+alpha)
+    f2=0.5*(1-alpha)
+    return f1*x+f2*tf.abs(x)
+
+def lrelu_outshape(input_shape):
+  return input_shape
 
 class BaseFeatureExtractor(object):
   def __init__(self,input_size):
@@ -16,32 +27,33 @@ class BaseFeatureExtractor(object):
 
 class TinyYolo(BaseFeatureExtractor):
   def __init__(self,input_size):
+    my_lrelu_layer=Lambda(lrelu,output_shape=lrelu_outshape)
     input_=Input(shape=(input_size,input_size,3))
-
+    
     #Layer 1
     x=Conv2D(16,(3,3),padding="same",use_bias=False)(input_)
     x=BatchNormalization()(x)
-    x=LeakyReLU(alpha=0.1)(x)
+    x=my_lrelu_layer(x)
     x=MaxPooling2D()(x)
 
     #Layer 2-5
     for i in range(4):
       x=Conv2D(32*(2**i),(3,3),padding="same",use_bias=False)(x)
       x=BatchNormalization()(x)
-      x=LeakyReLU(alpha=0.1)(x)
+      x=my_lrelu_layer(x)
       x=MaxPooling2D()(x)
 
     #Layer 6
     x=Conv2D(512,(3,3),padding="same",use_bias=False)(x)
     x=BatchNormalization()(x)
-    x=LeakyReLU(alpha=0.1)(x)
+    x=my_lrelu_layer(x)
     x=MaxPooling2D(strides=(1,1),padding="same")(x)
 
     #Layer 7-8
     for i in range(2):
       x=Conv2D(1024,(3,3),padding="same",use_bias=False)(x)
       x=BatchNormalization()(x)
-      x=LeakyReLU(alpha=0.1)(x)
+      x=my_lrelu_layer(x)
 
     self.feature_extractor=Model(input_,x)
   def normalize(self,img):
