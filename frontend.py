add BatchNormalization to fix Gradient Explosion

frontend.py
@@ -1,7 +1,7 @@
 from backend import TinyYolo
 import numpy as np
 import tensorflow as tf
-from keras.layers import Input,Conv2D,Lambda,Reshape
+from keras.layers import Input,Conv2D,Lambda,Reshape,BatchNormalization,Activation
 from keras.models import Model
 from keras.utils import multi_gpu_model
 from preprocessing import BatchGenerator
@@ -38,12 +38,14 @@ class YOLO(object):
       features=self.feature_extractor.extract(input_)
 
       output=Conv2D(self.nb_box*(4+1+self.nb_class),(1,1),strides=(1,1),padding="same")(features)
+      output=BatchNormalization()(output)
+      output=Activation("relu")(output)
       output=Reshape((self.grid_h,self.grid_w,self.nb_box,4+1+self.nb_class))(output)
       output=Lambda(lambda args:args[0])([output,self.true_boxes])
 
       self.orgmodel=Model([input_,self.true_boxes],output)
       
-      layer=self.orgmodel.layers[-4]
+      layer=self.orgmodel.layers[-6]
       weights=layer.get_weights()
       new_kernel=np.random.normal(size=weights[0].shape)/(self.grid_h*self.grid_w)
       new_bias=np.random.normal(size=weights[1].shape)/(self.grid_h*self.grid_w)
@@ -191,8 +193,8 @@ class YOLO(object):
     
     self.model.compile(loss=self.custom_loss,optimizer="adam")
     
-    early_stopping=EarlyStopping(monitor="val_loss",patience=50,mode="min",verbose=1)
-    checkpoint=ModelCheckpoint(saved_weights_name,monitor="val_loss",verbose=1,save_best_only=True,mode="min")
+    early_stopping=EarlyStopping(monitor="loss",patience=5,mode="min",verbose=1)
+    checkpoint=ModelCheckpoint(saved_weights_name,monitor="loss",verbose=1,save_best_only=True,mode="min")
     
     self.model.fit_generator(generator=train_generator,
                              steps_per_epoch=len(train_generator)*train_times,
@@ -213,7 +215,7 @@ class YOLO(object):
     dummy_array=np.zeros((1,1,1,1,self.max_box_per_img,4))
     
     netout=self.model.predict([input_img,dummy_array])[0]
-    boxes=decode_netout(netout,0.12,0.4,self.anchors,self.nb_class)
+    boxes=decode_netout(netout,0.3,0.4,self.anchors,self.nb_class)
     
     return boxes
     